080cf81797314371ff640048e8b955ca7c263dc5
Ho-seong Son <lsookcha@me.com>
2014-12-15 18:05:27 +0900

    급식정보 불러오기 완성

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,46 @@
 //= require_tree .
+
+$( document ).ready(function() {
+	  $.ajax({
+	    type: "GET",
+	    url: "http://cpsoft.kr/cpsoft/dimigo/food.php?d=20141215&output=xml",
+	    dataType: "xml",
+	    success: parseXml
+	  });
+		
+		function parseXml(xml)
+		{
+			var spinner = new Spinner().spin();
+			var target = document.getElementById('meal');
+			target.appendChild(spinner.el);
+			
+		  $(xml).find("breakfast").each(function()
+		  {
+		    console.log("아침 : " + $(this).text());
+				$(".breakfast").append($(this).text());
+		  });
+			
+		  $(xml).find("lunch").each(function()
+		  {
+		    console.log("점심 : " + $(this).text());
+				$(".lunch").append($(this).text());
+				
+		  });
+			
+		  $(xml).find("dinner").each(function()
+		  {
+		    console.log("저녁 : " + $(this).text());
+				$(".dinner").append($(this).text());
+				
+		  });
+			
+		  $(xml).find("snack").each(function()
+		  {
+		    console.log("간식 : " + $(this).text());
+				$(".snack").append($(this).text());
+				
+		  });
+			
+			spinner.stop();
+		}
+});
